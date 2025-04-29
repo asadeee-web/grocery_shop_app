1241079125f5b108e24437f7ea632781bf044ed4
Asad Qureshi <asadeee998@gmail.com>
2025-04-29 10:17:39 +0500

    added assets and also add some dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_shop_app/screens/home_screen.dart';
+import 'package:grocery_shop_app/screens/landing_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Grocery Shop',
       theme: ThemeData(),
-      home: HomeScreen(),
+      home: LandingScreen(),
     );
   }
 }
